09210feb335e882c94bb0024b9595dee9fbf74d8
prof Edney <111137546+profedney@users.noreply.github.com>
2023-04-25 15:07:02 -0300

    Delete multiplicador.py

--- multiplicador.py
+++
@@ -1,5 +0,0 @@
-print("Bem vindo ao multiplicador")
-num1=int(input("digite um numero inteiro: "))
-num2=int(input("digite outro numero inteiro: "))
-result=num1*num2
-print("O resultado da multiplicação é:",result)
