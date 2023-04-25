5bf42e10ca82cbe4c273ab9430f7a120b36d939c
prof Edney <111137546+profedney@users.noreply.github.com>
2023-04-25 15:07:55 -0300

    Add files via upload

---
+++ 3multiplicador.py
@@ -0,0 +1,5 @@
+print("Bem vindo ao multiplicador")
+num1=int(input("digite um numero inteiro: "))
+num2=int(input("digite outro numero inteiro: "))
+result=num1*num2
+print("O resultado da multiplicação é:",result)
