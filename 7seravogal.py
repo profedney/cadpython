282046eb2499652b78e8212ffdc5d772032bdf93
prof Edney <111137546+profedney@users.noreply.github.com>
2023-05-09 15:20:41 -0300

    Add files via upload

---
+++ 7seravogal.py
@@ -0,0 +1,16 @@
+#Faça um programa que peça para o usuario digitar uma letra 
+#e verifique se a letra é vogal ou consoante
+
+letrinha=input("Digite uma letra:\n")
+if letrinha=="a":
+    print("Você digitou uma vogal")
+elif letrinha=="e":
+    print("você digitou uma vogal")
+elif letrinha=="i":
+    print("você digitou uma vogal")
+elif letrinha=="o":
+    print("Você digitou uma vogal")
+elif letrinha=="u":
+    print("Você digitou uma vogal")
+else:
+    print("Você digitou uma consoante")
